11d0ce9a99483d756bfc7d5e46f148a0df37ba0d
anittapt <161876317+anittapt@users.noreply.github.com>
2025-03-09 20:54:56 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
-//practica_ut9/api.js
 
 const axios = require("axios");
 
-const API_URL = "https://catfact.ninja/fact";
+const API_URL = "https://catfact.ninja/fact";//URL de api publica cat facts
 
+// Función para obtener un dato aleatorio sobre gatos desde la API
 async function obtenerDatoGato() {
     try {
         const respuesta = await axios.get(API_URL);
-        return respuesta.data.fact;
+        return respuesta.data.fact;//extrae un dato sobre gatos
     } catch (error) {
         console.error("Error al obtener dato de la API:", error);
         return null;
@@ -15,6 +15,3 @@ async function obtenerDatoGato() {
 }
 
 module.exports = { obtenerDatoGato };
-
-
-
